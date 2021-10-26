09b9e046b2aaab246ef6e1c17bdbd5f34eb5f815
svidzger <german.svidzinskij@gmail.com>
2021-10-26 19:57:25 +0300

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,17 @@
 module.exports = {
   siteMetadata: {
     title: "My First Gatsby Site",
+    description: "Here is my learning process of React, GraphQl, Gatsby and web development in general.",
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
 };
---
+++ src/pages/blog.js
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My blog posts">
+      <ul>
+        {
+          data.allFile.nodes.map(node => (
+            <li key={node.id}>
+              {node.name}
+            </li>
+          ))
+        }
+      </ul>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query {
+    allFile {
+      nodes {
+        name
+        id
+      }
+    }
+  }
+`
+
+export default BlogPage
