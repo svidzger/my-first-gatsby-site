9adc37f43aa3345775daf4672942f59baa84fc5d
svidzger <german.svidzinskij@gmail.com>
2021-10-26 16:03:17 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Welcome to my first Gatsby website.</p>
+      <StaticImage
+        alt="Frog with butterfly."
+        src="../images/frog-with-butterfly.jpg"
+      />
     </Layout>
   )
 }
